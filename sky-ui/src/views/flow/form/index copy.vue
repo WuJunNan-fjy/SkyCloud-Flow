f44430da1c70d85e8e873184100ad5d99e66e136
吴某人15031370914 <2039495883@qq.com>
2024-12-20 11:48:38 +0800

    refactor(sky): 将 hasPermi 替换为 hasPermission
    - 在 controller.java.vm、index.vue、index-tree.vue 等多个文件中
    - 将 @PreAuthorize 和 v-hasPermi替换为 @PreAuthorize 和 v-hasPermission
    - 优化了代码的一致性和可读性

--- sky-ui/src/views/flow/form/index copy.vue
+++ sky-ui/src/views/flow/form/index copy.vue
@@ -17,7 +17,7 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
-                   v-hasPermi="['bpm:form:create']">新增</el-button>
+                   v-hasPermission="['bpm:form:create']">新增</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
@@ -40,11 +40,11 @@
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template v-slot="scope">
           <el-button size="mini" type="text" icon="el-icon-edit" @click="handleDetail(scope.row)"
-                     v-hasPermi="['bpm:form:query']">详情</el-button>
+                     v-hasPermission="['bpm:form:query']">详情</el-button>
           <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)"
-                     v-hasPermi="['bpm:form:update']">修改</el-button>
+                     v-hasPermission="['bpm:form:update']">修改</el-button>
           <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
-                     v-hasPermi="['bpm:form:delete']">删除</el-button>
+                     v-hasPermission="['bpm:form:delete']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
